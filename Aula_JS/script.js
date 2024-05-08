d9bed23de73942890b7a118fe37533f18b9d798d
Allan <69430214+allanlealluz@users.noreply.github.com>
2024-05-08 14:52:40 -0300

    changes 08/05 4

--- Aula_JS/script.js
+++ Aula_JS/script.js
@@ -1,3 +1,12 @@
 function hora(){
-    document.getElementById('demo').innetHTML = Date();
+    document.getElementById('demo').innerHTML = Date();
+}
+function troca(){
+document.getElementById('demo').innerHTML = "Ola Javascript!";
+}
+function troca1(){
+document.getElementById('myImage').src;
+}
+function troca2(){
+    document.getElementById('myImage').src;
 }
