72fd55e66b3ec2ec7e1663c8fbaa6596268d631c
Allan <69430214+allanlealluz@users.noreply.github.com>
2024-05-08 14:58:18 -0300

    changes 08/05 5

--- Aula_JS/script.js
+++ Aula_JS/script.js
@@ -5,8 +5,8 @@ function troca(){
 document.getElementById('demo').innerHTML = "Ola Javascript!";
 }
 function troca1(){
-document.getElementById('myImage').src;
+document.getElementById('myImage').src = 'imagens/acesa.jpg'; 
 }
 function troca2(){
-    document.getElementById('myImage').src;
+    document.getElementById('myImage').src= 'imagens/apagada.jpg';
 }
