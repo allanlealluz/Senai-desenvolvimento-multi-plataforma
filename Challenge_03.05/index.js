12f8cdce7e0a45102c34554066a5400999a6e30b
Allan <69430214+allanlealluz@users.noreply.github.com>
2024-05-08 16:40:45 -0300

    changes 08/05 7

---
+++ Challenge_03.05/index.js
@@ -0,0 +1,103 @@
+function Historico() {
+    var divMain = document.getElementById('main');
+    divMain.innerHTML = `
+    <div class="container d-flex flex-column history justify-content-start">
+       <h2 class="text-light">Histórico de Exibição</h2>
+       <h3 class="text-light">Hoje</h3>
+       <div class="card bg-dark border-dark" style="width: 18rem;">
+       <div class="card-body">
+       <img alt="" style="background-color: transparent;" class="yt-core-image yt-core-image--fill-parent-height yt-core-image--fill-parent-width yt-core-image--content-mode-scale-aspect-fill yt-core-image--loaded" src="https://i.ytimg.com/vi/8mczzifNRc0/hqdefault.jpg?sqp=-oaymwE2CPYBEIoBSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgB_gmAAtAFigIMCAAQARhlIGUoZTAP&amp;rs=AOn4CLDD2RAKhusg5-Gim-yaj0XcgZZUog">
+           <h6 class="card-title text-center" style="color:white;">GT4 THEME</h5>
+       </div>
+       </div>
+    `;
+}
+function Home(){
+    var div = document.getElementById('main');
+    div.innerHTML = `
+    <div>
+    <span class="badge big-badge"><h5>Tudo</h5></span>
+    <span class="badge big-badge"><h5>Mix</h5></span>
+    <span class="badge big-badge"><h5>Jogos</h5></span>
+    <span class="badge big-badge"><h5>Dark Ambience</h5></span>
+    <span class="badge big-badge"><h5>Fnaf</h5></span>
+    <span class="badge big-badge"><h5>Core</h5></span>
+    <span class="badge big-badge"><h5>Stella Blade</h5></span>
+    <span class="badge big-badge"><h5>League of Legends</h5></span>
+    <span class="badge big-badge"><h5>Plants vs Zombies</h5></span>
+</div>
+<div class="card-group">
+<div class="card bg-dark border-dark" style="width: 18rem;">
+<div class="card-body">
+    <img src="img/download (1).jpg" alt="" class="card-img-top">
+    <h6 class="card-title text-center" style="color:white;">A IA irá nos substituir</h5>
+</div>
+</div>
+<div class="card bg-dark border-dark" style="width: 18rem;">
+    <div class="card-body">
+        <img alt="" style="background-color: transparent;" class="card-img-top yt-core-image yt-core-image--fill-parent-height yt-core-image--fill-parent-width yt-core-image--content-mode-scale-aspect-fill yt-core-image--loaded" src="https://i.ytimg.com/vi/bva2KUuER6s/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&amp;rs=AOn4CLBks0l8ZCuxR7ue5RzliY34YaLucA">
+        <h6 class="card-title text-center" style="color:white;">Como não ligar para a opinião dos outros, Modo Sério</h5>
+    </div>
+    </div>
+</div>
+<div class="card-group low">
+<div class="card bg-dark border-dark" style="width: 18rem;">
+<div class="card-body">
+    <img alt="" style="background-color: transparent;" class="yt-core-image yt-core-image--fill-parent-height yt-core-image--fill-parent-width yt-core-image--content-mode-scale-aspect-fill yt-core-image--loaded card-img-top" src="https://i.ytimg.com/vi/h5L_Aqkp-Gg/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&amp;rs=AOn4CLD0l-t9xSeyDrxKR6YaGTQFitIqMA">
+    <h6 class="card-title text-center" style="color:white;">VENI, VIDI, VICI |1 hora de Ambiente Romano</h5>
+</div>
+</div>
+<div class="card bg-dark border-dark" style="width: 18rem;">
+    <div class="card-body">
+        <img alt="" style="background-color: transparent;" class="card-img-top yt-core-image yt-core-image--fill-parent-height yt-core-image--fill-parent-width yt-core-image--content-mode-scale-aspect-fill yt-core-image--loaded" src="https://i.ytimg.com/vi/Sie6JcRTMtY/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&amp;rs=AOn4CLD5LdMi3co9xDniEa7-2TQZzrtT_A">
+        <h6 class="card-title text-center" style="color:white;">O primeiro serial killer da história dos estados unidos H.H Homes</h5>
+    </div>
+    </div>
+    `;
+}
+function Shorts(){
+    var div = document.getElementById('main');
+    div.innerHTML = `
+    <div class="container-fluid">
+<img alt="" style="background-color: transparent;" class="img-fluid full_img" src="https://i.ytimg.com/vi/XpAUQlO7VS0/hq720.jpg?sqp=-oaymwE2CNAFEJQDSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgBzgWAAtAFigIMCAAQARhNIBMofzAP&amp;rs=AOn4CLCWareIFj2WRQsEGAvAFkSgBMH1LA">
+<h3 class="text-light full_text">Sea of Problemns</h3>
+    </div>
+`
+}
+function Inscrições(){
+    var div = document.getElementById('main');
+    div.innerHTML = `
+    <div class="container-fluid">
+    <h2 class="text-light">Mais Recentes </h2>
+    <div class="card-group">
+<div class="card bg-dark border-dark" style="width: 18rem;">
+<div class="card-body">
+<img alt="" style="background-color: transparent;" class="card-img-top yt-core-image yt-core-image--fill-parent-height yt-core-image--fill-parent-width yt-core-image--content-mode-scale-aspect-fill yt-core-image--loaded" src="https://i.ytimg.com/vi/hhbKZsTCNdg/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&amp;rs=AOn4CLDqkT-GsTEkGQ_dzpQl7dxqVw4p-g">
+    <h6 class="card-title text-center" style="color:white;">SUPER XANDÃO 190 Q.I - OS CÉUS PROCLAMAM A GLÓRIA DE DEUS E O FIRMAMENTO ANUNCIA A OBRA DE SUAS MÃOS</h5>
+</div>
+</div>
+<div class="card bg-dark border-dark" style="width: 18rem;">
+    <div class="card-body">
+        <img alt="" style="background-color: transparent;" class="card-img-top yt-core-image yt-core-image--fill-parent-height yt-core-image--fill-parent-width yt-core-image--content-mode-scale-aspect-fill yt-core-image--loaded" src="https://i.ytimg.com/vi/bva2KUuER6s/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&amp;rs=AOn4CLBks0l8ZCuxR7ue5RzliY34YaLucA">
+        <h6 class="card-title text-center" style="color:white;">Como não ligar para a opinião dos outros, Modo Sério</h5>
+    </div>
+    </div>
+</div>
+   
+    `
+}
+function AssistirTarde(){
+    var div = document.getElementById('main');
+    div.innerHTML = `
+    <div class="container">
+    <h2>Assistir Mais Tarde</h2>
+    </div>
+    <div class="card bg-dark border-dark" style="width: 18rem;">
+    <div class="card-body">
+        <img alt="" style="background-color: transparent;" class="card-img-top yt-core-image yt-core-image--fill-parent-height yt-core-image--fill-parent-width yt-core-image--content-mode-scale-aspect-fill yt-core-image--loaded" src="https://i.ytimg.com/vi/bva2KUuER6s/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&amp;rs=AOn4CLBks0l8ZCuxR7ue5RzliY34YaLucA">
+        <h6 class="card-title text-center" style="color:white;">Como não ligar para a opinião dos outros, Modo Sério</h5>
+    </div>
+    </div>
+</div>
+    `
+}
