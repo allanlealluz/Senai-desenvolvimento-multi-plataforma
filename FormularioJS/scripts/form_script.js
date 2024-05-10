0d5ec0cb1fec3ce8a8bb47390372196930057117
Allan <69430214+allanlealluz@users.noreply.github.com>
2024-05-10 16:04:10 -0300

    changes 10/05 3

--- FormularioJS/scripts/form_script.js
+++ FormularioJS/scripts/form_script.js
@@ -1,9 +1,12 @@
 function Validar(){
-    if(document.forms.nome.value.length == 0 || document.forms.email.value.length == 0 || document.forms.senha.value.length == 0){
+    if(forms.nome.value.length == 0 || forms.email.value.length == 0 || forms.senha.value.length == 0){
         alert('Há campos vazios, favor preencher');
         return 0;
+    }else if(forms.nome.value.length < 10){
+        alert('Preencha com seu nome completo')
+        return 0;
     }
-    if(document.forms.senha.value != document.forms.rsenha.value){
+    if(forms.senha.value != forms.rsenha.value){
         alert("As senhas são diferentes");
         return 0;
     }
