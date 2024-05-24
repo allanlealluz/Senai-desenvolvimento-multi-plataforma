9e858e035f6d09baba04ae310ec5183a443fdbf3
Allan <69430214+allanlealluz@users.noreply.github.com>
2024-05-24 16:55:12 -0300

    changes 24/05

--- Bootstrap 4/FormularioJS/scripts/meu_script.js
+++
@@ -1,8 +0,0 @@
-function valida_envia(){
-///validação de nome 
-   if(document.fvalida.nome.value.length == 0){
-    alert("Por favor, digite seu nome!");
-    document.fvalida.nome.focus();
-    return 0;
-   }
-}
