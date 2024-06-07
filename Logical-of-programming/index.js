e9cc9e610e3863474b3382190a5e9668794054b5
Allan <69430214+allanlealluz@users.noreply.github.com>
2024-06-07 14:20:08 -0300

    changes 07/06 5

--- Logical-of-programming/index.js
+++ Logical-of-programming/index.js
@@ -3,8 +3,14 @@ function SomarNumbers(x,y){
     return x+y;
 }
 //Solitação para o Usuario
+/*
+let z = SomarNumbers(x,y);
+console.log(`A soma é: ${z}`);
+alert(z)
+*/
+//Solitação para o Usuario
 let x = parseInt(prompt('digite um valor: '));
 let y = parseInt(prompt('digite outro valor: '));
-z = x+y;
+z = x/y;
 console.log(`A soma é: ${z}`);
 alert(z)
