ff7165db26856e0404dc0895ec3ba06e4b05d521
Allan <69430214+allanlealluz@users.noreply.github.com>
2024-06-12 14:28:43 -0300

    changes 12/06 5

--- Logical-of-programming/12Juny/index.js
+++ Logical-of-programming/12Juny/index.js
@@ -1,4 +1,9 @@
-var nome = prompt('digite seu nome:');
+/*var nome = prompt('digite seu nome:');
 console.log('Ola '+nome);
 document.write('<h1>Bem Vindo, '+nome+' </h1>');
 document.write("<img src='https://sujeitoprogramador.com/steve.png' alt='foto do steve jobs'/>")
+*/
+var area = document.getElementById('area');
+function entrar(){
+    
+}
