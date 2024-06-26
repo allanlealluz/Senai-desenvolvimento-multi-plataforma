d521bc2c9a7f8a5d4ba98273e0863b598919baf0
TRIZ <luiz.rech@conffops.com.br>
2024-06-26 14:15:54 -0300

    changes 26/06 2

--- LISTAS_JS/script.js
+++ LISTAS_JS/script.js
@@ -3,3 +3,16 @@ let frutas = ["Maça","Banana","Laranja","Pera"];
 
 //Metódo construtor
 let carros = new Array("Fiat","Toyota","Honda");
+
+//Acesso Elementos
+let fruta01 = frutas[0];
+let fruta02 = frutas[1];
+let fruta03 = frutas[2];
+let fruta04 = frutas[3];
+
+//Modificando Elementos
+frutas[0] = "Uva";
+frutas.push('Uva') // adiciona elemento no final da lista
+frutas.unshift('Uva') // adiciona elemento no inicio da lista
+frutas.pop() // remove o ultimo elemento da lista
+frutas.shift() // remove o primeiro elemento da lista
