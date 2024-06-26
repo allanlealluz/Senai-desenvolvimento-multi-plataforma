372cf1404e667e26d08b16d52bbfeca8759560e6
TRIZ <luiz.rech@conffops.com.br>
2024-06-26 18:52:23 -0300

    changes 26/06 6

--- LISTAS_JS/exercicio.js
+++ LISTAS_JS/exercicio.js
@@ -6,15 +6,15 @@ function Sum(value){
     return sum;
 }
 function filterPar(value){
-    newArray = value.filter((item){
-        return numero % 2 === 0;
+    newArray = value.filter((item)=>{
+        return item % 2 === 0;
     })
     return newArray;
 }
 function Invert(value){
     invert = [];
     for(let i = 0; i < value.length; i++){
-        invert.push(value[i] * -1)
+        invert.unshift(value[i])
     }
     return invert;
 }
@@ -26,8 +26,8 @@ function verifyDuplicate(value){
     novaArr = value.filter((este,i)=>value.indexOf(este)===i);
     return novaArr;
 }
-console.log(sum([1,2,3,54,8]));
+console.log(Sum([1,2,3,54,8]));
 console.log(filterPar([1,2,3,54,8]));
 console.log(Invert([1,2,3,54,8]));
-    console.log(Concat([1,2,3,54,8],[1,2,3,54,8]));
-    console.log(verifyDuplicate([1,2,3,54,8]));
+console.log(Concat([1,2,3,54,8],[1,22,5,54,7]));
+console.log(verifyDuplicate([1,2,3,54,8]));
