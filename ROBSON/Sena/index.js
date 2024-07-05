ee2b7d1cdd344edcba8485d3630754990251c9d4
Aleph Goldstein <luiz.rech@conffops.com.br>
2024-07-05 14:58:36 -0300

    changes 05/07 2

--- ROBSON/Sena/index.js
+++ ROBSON/Sena/index.js
@@ -3,28 +3,46 @@ function Sena(){
     let i = 0
     while(i < 60){
             let RandNum = (Math.floor(Math.random()*60)+1)
-            if(RandNum !== 0 ){ 
             if(!valores.includes(RandNum)){
                 valores.push(RandNum)
                 i++
-            }
-        }
-         
+            }  
     }
     MostraValores(valores)
+    VerificarAcerto()
 }
 function MostraValores(valores){
     result = document.getElementById('result')
     num = 0
+    count = 0
     for(let i = 0; i< 10; i++){
         tr = document.createElement('tr')
-        tr.setAttribute('id',"tr"+num)
+        tr.setAttribute('id',"tr"+count)
+        count ++
         for(let j = 0; j < 6; j++){
             td = document.createElement('td')
-            td.innerHTML = "||"+valores[num]+"||"
+            td.innerHTML = "|| "+valores[num]+" ||"
             tr.appendChild(td)
             num ++
         }
             result.appendChild(tr)
     }
+}
+function VerificarAcerto(){
+    acertos = 0
+    aposta = document.getElementById("aposta").value
+    resultAcertos = document.getElementById('acertos')
+    for(let i = 0; i < 10; i++){
+        tr = document.getElementById("tr"+i).innerHTML
+        var numsStr = tr.replace(/[^0-9]/g,'');
+        console.log(parseInt(numsStr));
+        if(aposta == numsStr){
+            alert("Parabéns, você acertou o número da sorte!"+aposta)
+            acertos ++
+        }else{
+            console.log("Não foi dessa vez, tente novamente!")
+        }
+        resultAcertos.innerHTML += " "+acertos+" "
+    }
+
 }
