7c6e8147ebb3d2a553692fcb99aaa13f504a1e92
Aleph Goldstein <luiz.rech@conffops.com.br>
2024-07-10 13:54:22 -0300

    .

--- primeiro_app2/src/routes/rout.jsx
+++ primeiro_app2/src/routes/rout.jsx
@@ -1,28 +1,11 @@
+import image from "./image.png"
 export default function Root(){
     return (
-        <div id="sidebar">
-            <div id="sidebar-content">
-                <div id="sidebar-header">
-                    <h1>Allone</h1>
-                    <h2>My Sidebar</h2>
-                    
-                    <div id="sidebar-actions">
-                        <a href="#" class="sidebar-close"><i class="fas fa-times"></i>aa</a>
-                        <a href="#" class="sidebar-toggle"><i class="fas fa-bars"></i>aa</a>                    
-                    </div>
-                </div>
-            </div>
-            <form>
-                <div class="form-group">
-                    <label for="name">Name</label>
-                    <input type="text" class="form-control" id="name" name="name"></input>
-                </div>
-            </form>
-            
-            <div class="sidebar-footer">
-                <a href="https://github.com/brunocarvalho92" target="_blank"><i class="fab fa-github"></i>aa</a>
-                <a href="https://www.linkedin.com/in/brunocarvalho92/" target="_blank"><i class="fab fa-linkedin"></i>aa</a>
-            </div>
+        <div id="conteudo">
+           <div className="login-page">
+               <img src={image} alt="logo"></img>
+                <p>Para acessar o sistema, faça login</p>
+           </div>
         </div>
     )
 }
